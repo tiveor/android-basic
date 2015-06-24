5d2e8a36026fcab66257ab39ecf390117ff86c5e
Alvaro Orellana <tiveor@gmail.com>
2015-06-23 20:10:49 -0400

    Content Provider Test Project Fixes

--- SharedPreferenceTest/app/src/main/java/com/possiblelabs/sharedpreferencetest/LoginActivity.java
+++ SharedPreferenceTest/app/src/main/java/com/possiblelabs/sharedpreferencetest/LoginActivity.java
@@ -18,12 +18,13 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-
         Button btn = (Button) findViewById(R.id.btn_login);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PreferenceUtil.setLoggedIn(LoginActivity.this, true);
+
                 Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
