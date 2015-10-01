d9f6e0bd4909192d38bc01d0143ae084c4e6efb8
Alvaro Orellana <tiveor@gmail.com>
2015-10-01 18:19:41 -0400

    SQLiteTest Updated

--- SQLiteTest/app/src/main/java/com/possiblelabs/sqlitetest/MainActivity.java
+++ SQLiteTest/app/src/main/java/com/possiblelabs/sqlitetest/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +12,7 @@ import com.possiblelabs.sqlitetest.fragment.PlaceHolderFragment;
 import com.possiblelabs.sqlitetest.fragment.TodoListFragment;
 
 
-public class MainActivity extends AppCompatActivity implements PlaceHolderFragment.NavigationDrawerFragment.NavigationDrawerCallbacks, Constants {
+public class MainActivity extends ActionBarActivity implements PlaceHolderFragment.NavigationDrawerFragment.NavigationDrawerCallbacks, Constants {
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
