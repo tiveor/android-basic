8b5fa01652abbf828b93f412a48768b0dcd9d0a8
Alvaro Orellana <tiveor@gmail.com>
2015-10-23 20:27:26 -0400

    GraphicsTest

---
+++ GraphicsTest/app/src/main/java/com/possiblelabs/graphicstest/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.possiblelabs.graphicstest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by possiblelabs on 10/23/15.
+ */
+public class SecondActivity extends Activity {
+
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+    }
+}
